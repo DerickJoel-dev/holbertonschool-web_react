d17eea1f35074fbe0f1f08f34cb3dc39012ef5eb
DerickJoel-dev <derickdev2@gmail.com>
2025-06-01 14:48:47 -0400

    Brand convention & Nominal typing

---
+++ TypeScript/task_5/js/main.ts
@@ -0,0 +1,40 @@
+// Nominal typing using branding
+
+export interface MajorCredits {
+  credits: number;
+  readonly __brand: 'MajorCredits';
+}
+
+export interface MinorCredits {
+  credits: number;
+  readonly __brand: 'MinorCredits';
+}
+
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MajorCredits',
+  };
+}
+
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MinorCredits',
+  };
+}
+
+
+const major1: MajorCredits = { credits: 3, __brand: 'MajorCredits' };
+const major2: MajorCredits = { credits: 4, __brand: 'MajorCredits' };
+console.log(sumMajorCredits(major1, major2)); 
+
+const minor1: MinorCredits = { credits: 2, __brand: 'MinorCredits' };
+const minor2: MinorCredits = { credits: 1, __brand: 'MinorCredits' };
+console.log(sumMinorCredits(minor1, minor2));
